f3707c69ea30397d6d040f74ce05cc034b538e04
Victor Alexander Yttesen <victor.yttesen@tutanota.com>
2025-10-19 01:36:18 +0200

    Fixed two errors (probably due to update in PyTorch API). One regarding bias in conv2d forward and one change from .view to .reshape

--- quan/func.py
+++ quan/func.py
@@ -4,13 +4,17 @@ import torch as t
 class QuanConv2d(t.nn.Conv2d):
     def __init__(self, m: t.nn.Conv2d, quan_w_fn=None, quan_a_fn=None):
         assert type(m) == t.nn.Conv2d
-        super().__init__(m.in_channels, m.out_channels, m.kernel_size,
-                         stride=m.stride,
-                         padding=m.padding,
-                         dilation=m.dilation,
-                         groups=m.groups,
-                         bias=True if m.bias is not None else False,
-                         padding_mode=m.padding_mode)
+        super().__init__(
+            m.in_channels,
+            m.out_channels,
+            m.kernel_size,
+            stride=m.stride,
+            padding=m.padding,
+            dilation=m.dilation,
+            groups=m.groups,
+            bias=True if m.bias is not None else False,
+            padding_mode=m.padding_mode,
+        )
         self.quan_w_fn = quan_w_fn
         self.quan_a_fn = quan_a_fn
 
@@ -18,18 +22,21 @@ class QuanConv2d(t.nn.Conv2d):
         self.quan_w_fn.init_from(m.weight)
         if m.bias is not None:
             self.bias = t.nn.Parameter(m.bias.detach())
+        else:
+            self.bias = None
 
     def forward(self, x):
         quantized_weight = self.quan_w_fn(self.weight)
         quantized_act = self.quan_a_fn(x)
-        return self._conv_forward(quantized_act, quantized_weight)
+        return self._conv_forward(quantized_act, quantized_weight, self.bias)
 
 
 class QuanLinear(t.nn.Linear):
     def __init__(self, m: t.nn.Linear, quan_w_fn=None, quan_a_fn=None):
         assert type(m) == t.nn.Linear
-        super().__init__(m.in_features, m.out_features,
-                         bias=True if m.bias is not None else False)
+        super().__init__(
+            m.in_features, m.out_features, bias=True if m.bias is not None else False
+        )
         self.quan_w_fn = quan_w_fn
         self.quan_a_fn = quan_a_fn
 
@@ -44,7 +51,4 @@ class QuanLinear(t.nn.Linear):
         return t.nn.functional.linear(quantized_act, quantized_weight, self.bias)
 
 
-QuanModuleMapping = {
-    t.nn.Conv2d: QuanConv2d,
-    t.nn.Linear: QuanLinear
-}
+QuanModuleMapping = {t.nn.Conv2d: QuanConv2d, t.nn.Linear: QuanLinear}
