ab7429c3ba02a3736e5e568b86ddb9a22cfeaac2
Victor Alexander Yttesen <victor.yttesen@tutanota.com>
2025-10-19 18:41:40 +0200

    Added support for multiple different grad scales

--- quan/quantizer/lsq.py
+++ quan/quantizer/lsq.py
@@ -16,39 +16,54 @@ def round_pass(x):
 
 
 class LsqQuan(Quantizer):
-    def __init__(self, bit, all_positive=False, symmetric=False, per_channel=True):
+    def __init__(
+        self, bit, grad_scale, all_positive=False, symmetric=False, per_channel=True
+    ):
         super().__init__(bit)
 
         if all_positive:
             assert not symmetric, "Positive quantization cannot be symmetric"
             # unsigned activation is quantized to [0, 2^b-1]
             self.thd_neg = 0
-            self.thd_pos = 2 ** bit - 1
+            self.thd_pos = 2**bit - 1
         else:
             if symmetric:
                 # signed weight/activation is quantized to [-2^(b-1)+1, 2^(b-1)-1]
-                self.thd_neg = - 2 ** (bit - 1) + 1
+                self.thd_neg = -(2 ** (bit - 1)) + 1
                 self.thd_pos = 2 ** (bit - 1) - 1
             else:
                 # signed weight/activation is quantized to [-2^(b-1), 2^(b-1)-1]
-                self.thd_neg = - 2 ** (bit - 1)
+                self.thd_neg = -(2 ** (bit - 1))
                 self.thd_pos = 2 ** (bit - 1) - 1
 
         self.per_channel = per_channel
+        self.grad_scale = grad_scale
         self.s = t.nn.Parameter(t.ones(1))
 
     def init_from(self, x, *args, **kwargs):
         if self.per_channel:
             self.s = t.nn.Parameter(
-                x.detach().abs().mean(dim=list(range(1, x.dim())), keepdim=True) * 2 / (self.thd_pos ** 0.5))
+                x.detach().abs().mean(dim=list(range(1, x.dim())), keepdim=True)
+                * 2
+                / (self.thd_pos**0.5)
+            )
         else:
-            self.s = t.nn.Parameter(x.detach().abs().mean() * 2 / (self.thd_pos ** 0.5))
+            self.s = t.nn.Parameter(x.detach().abs().mean() * 2 / (self.thd_pos**0.5))
 
     def forward(self, x):
         if self.per_channel:
+            assert self.grad_scale == "full", (
+                "only full grad scale supported with per_channel=True"
+            )
             s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
+        elif self.grad_scale == "full":
+            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
+        elif self.grad_scale == "weights":
+            s_grad_scale = 1.0 / ((x.numel()) ** 0.5)
+        elif self.grad_scale == "none":
+            s_grad_scale = 1
         else:
-            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
+            raise ValueError("grad_scale must be in ['full', 'weights', 'none']")
         s_scale = grad_scale(self.s, s_grad_scale)
 
         x = x / s_scale
